0259731f35dda984db5a68683b115405b0e58f6d
Martha Nieto <m27842n@outlook.com>
2021-12-15 18:04:39 -0800

    Update: added credentials with cookies.

--- servers/node_server/server.js
+++ servers/node_server/server.js
@@ -7,16 +7,19 @@ app.use(cookieParser());
 app.use(morgan('tiny'));
 
 let f = (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "http://localhost:3030");
+    res.header("Access-Control-Allow-Credentials", "true");
+    console.log(req.cookies);
     res.send({
         hola: "mundo"
     });
 }
 
 app.options("/", (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "http://localhost:3030");
     res.header("Access-Control-Allow-Methods", "PUT");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authentication");
+    res.header("Access-Control-Allow-Credentials", "true");
     res.status(204).send();
 })
 
